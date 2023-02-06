2ccbe060dc299c969ea2f4ebff0f9c0dd64f5d9f
Charles Edward Pax <charles.pax@gmail.com>
2023-02-06 11:47:35 -0500

    Added print.sh, which prints the text fifty lines at a time to the default printer.

---
+++ notes/print.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# check if an argument was passed
+if [ $# -eq 0 ]; then
+  echo "Error: No argument provided"
+  exit 1
+fi
+
+# store the argument as a variable
+file=$1
+
+# set the default number of lines to 20
+lines=50
+
+# check if a second argument was passed
+if [ $# -eq 2 ]; then
+  lines=$2
+fi
+
+# check if the argument is a file
+if [ ! -f "$file" ]; then
+  echo "Error: First argument is not a file"
+  exit 1
+fi
+
+# add line numbers to the file
+nl -b a $file > $file.numbered
+
+# split the file into groups of the specified number of lines and print each group
+split -l $lines $file.numbered -d -a 4 print_
+for i in print_*; do
+  lp -o cpi=14 -o lpi=7.5 -o page-bottom=72 -o page-left=72 -o page-right=72 -o page-top=72 $i
+done
+
+# delete the temporary files
+rm print_*
+rm $file.numbered
+
+# exit the script with a success status code
+exit 0
+
